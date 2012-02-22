4d4e9d7b24fe370741bab74b60edd15f275f1868
bbx10node <bbx10node@gmail.com>
2012-02-22 00:05:20 -1000

    Show all 256 addresses

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 var app = require('http').createServer(handler),
-    io = require('socket.io').listen(app),
-    fs = require('fs'),
-    net = require('net');
+io = require('socket.io').listen(app),
+fs = require('fs'),
+net = require('net');
 // Connect to mochad on port 1099.
-var mochad = net.createConnection(1099, 'beaglebone');
+var mochad = net.createConnection(1099, 'bbxm');
 var x10state = require('./x10state.js');
 
 // Start http server
@@ -17,59 +17,160 @@ function handler (req, res)
 {
     //console.log(req.url);
     fs.readFile(__dirname + '/buttons.html',
-            function (err, data) {
-            if (err) {
-            res.writeHead(500);
-            return res.end('Error loading buttons.html');
-            }
+                function (err, data) {
+                    if (err) {
+                        res.writeHead(500);
+                        return res.end('Error loading buttons.html');
+                    }
 
-            res.writeHead(200);
-            res.end(data);
-            });
+                    res.writeHead(200);
+                    res.end(data);
+                });
 }
 
-function mochad_on_connect(data)
+var LastEvent='';
+
+function process_line(that, str)
 {
+    var words, washernormal, washeralert;
+    var x10addr, housecode, unitcode, i, unitstatus, unitparts, x10func;
+    var buttonupdate;
+
+    if (str.match(/Device selected/)) {
+        console.log('DevSel');
+        //that.DevSel = true;
+    }
+    else if (str.match(/Device status/)) {
+        console.log('DevStat');
+        that.DevSection = "devstat";
+    }
+    else if (str.match(/Security sensor status/)) {
+        // 01/14 20:18:37 Security sensor status
+        that.DevSection = "sensors";
+    }
+    else if (str.match(/End status/)) {
+        console.log('devend');
+        that.DevSection = "";
+    }
+    else if (that.DevSection === "devsel") {
+        console.log('device selected');
+        //01/02 10:45:45 House A: 2,14
+        words = str.split(" ");
+        if (words[2] === "House") {
+            housecode = words[3].slice(0,1);
+            unitstatus = words[4].split(",");
+            for (i = 0; i < unitstatus.length; i++) {
+                x10state.select(housecode + unitstatus[i]);
+            }
+        }
+    }
+    else if (that.DevSection === "devstat") {
+        console.log('device status');
+        //01/02 11:11:35 House L: 4=0,12=0,14=1
+        words = str.split(" ");
+        if (words[2] === "House") {
+            housecode = words[3].slice(0,1);
+            //console.log('DevStat ' + housecode);
+            unitstatus = words[4].split(",");
+            for (i = 0; i < unitstatus.length; i++) {
+                //console.log('DevStat ' + unitstatus[i]);
+                unitparts = unitstatus[i].split("=");
+                unitcode = unitparts[0];
+                //console.log('DevStat ' + unitparts);
+                switch (unitparts[1]) {
+                    case "0":
+                        x10state.off(housecode + unitcode);
+                    break;
+                    case "1":
+                        x10state.on(housecode + unitcode);
+                    break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    else if (str.match(/[TR]x PL HouseUnit/)) {
+        //01/01 22:11:42 Rx PL HouseUnit: C2
+        //console.log('HouseUnit:');
+        words = str.split(" ");
+        x10state.select(words[5]);
+    }
+    else if (str.match(/[TR]x PL House:/)) {
+        //01/01 22:11:42 Rx PL House: C Func: On
+        //01/02 13:07:52 Tx PL House: E Func: All units off
+        //console.log('House:');
+        if (LastEvent !== str) {
+            LastEvent = str;
+            words = str.split(" ");
+            x10func = words[7];
+            for (i = 8; i < words.length; i++) {
+                x10func = x10func + "_" + words[i];
+            }
+            io.sockets.emit('buttonupdate', x10state.doselected(words[5], x10func) );
+        }
+    }
+}
+
+mochad.on('connect', function() {
+    this.on('end', function() {
+        console.log('on end');
+        this.end();
+    });
+    this.on('error', function() {
+        console.log('on error');
+        this.end();
+    });
+    this.on('data', function(data) {
+        var str = data.toString(), lines, aLine, i;
+        console.log('on data');
+        if (this.PartialLine !== "") {
+            str = this.PartialLine + str;
+        }
+        lines = str.split('\n');
+        for (i = 0; i < (lines.length - 1); i++) {
+            aLine = lines[i];
+            console.log(aLine);
+            process_line(this, aLine);
+        }
+        if (str.slice(-1) === '\n') {
+            process_line(this, lines[i]);
+            this.PartialLine = "";
+        }
+        else {
+            // Save partial line for next time
+            this.PartialLine = lines[i];
+        }
+    });
     console.log('on connect');
     this.PartialLine = "";
     this.DevSection = "";
-}
-
-function mochad_on_end(data)
-{
-    console.log('on end');
-    this.end();
-}
-
-// For simplicity, input from mochad is ignored.
-mochad.on('connect', mochad_on_connect);
-mochad.on('end', mochad_on_end);
+    this.write("st\r");
+});
 
 io.set('log level', 1); // reduce socket.io logging
 
 // This function runs whenever a socket.io client connects.
 io.sockets.on('connection', function (socket) {
-        socket.emit('buttonupdate', x10state.now());
+    socket.emit('buttonupdate', x10state.now());
 
-        // This function runs when a button is pressed on the web client.
-        socket.on('buttonpress', function (data) {
-            console.log('on socket buttonpress ' + data.button);
-            if (x10state.state(data.button) === 'off') {
+    // This function runs when a button is pressed on the web client.
+    socket.on('buttonpress', function (data) {
+        console.log('on socket buttonpress ' + data.button);
+        if (x10state.state(data.button) === 'off') {
             x10state.on(data.button);
-            socket.emit('buttonupdate', [ { buttonid: data.button, state: 'on' } ] );
-            socket.broadcast.emit('buttonupdate', [ { buttonid: data.button, state: 'on' } ] );
+            io.sockets.emit('buttonupdate', [ { buttonid: data.button, state: 'on' } ] );
             // Turn the light on using mochad
             mochad.write('pl ' + data.button + ' on\n');
-            }
-            else if (x10state.state(data.button) === 'on') {
+        }
+        else if (x10state.state(data.button) === 'on') {
             x10state.off(data.button);
-            socket.emit('buttonupdate', [ { buttonid: data.button, state: 'off' } ] );
-            socket.broadcast.emit('buttonupdate', [ { buttonid: data.button, state: 'off' } ] );
+            io.sockets.emit('buttonupdate', [ { buttonid: data.button, state: 'off' } ] );
             // Turn the light off
             mochad.write('pl ' + data.button + ' off\n');
-            }
-            });
-        // This function runs when a web client disconnects.
-        socket.on('end', function(socket) {
-            });
+        }
+    });
+    // This function runs when a web client disconnects.
+    socket.on('end', function(socket) {
+    });
 });
--- x10state.js
+++ x10state.js
@@ -2,16 +2,17 @@
 
 'use strict';
 var x10state = {};
+var X10addrselected = [];
 
 exports.on = function(x10addr)
 {
     x10state[x10addr] = 'on';
-}
+};
 
 exports.off = function(x10addr)
 {
     x10state[x10addr] = 'off';
-}
+};
 
 exports.state = function(x10addr)
 {
@@ -21,7 +22,109 @@ exports.state = function(x10addr)
     else {
         return 'off';
     }
+};
+
+function housecodetoint(housecode)
+{
+    return ("ABCDEFGHIJKLMNOP".indexOf(housecode));
 }
+
+exports.select = function(x10addr)
+{
+    var housecode, unitcode;
+    housecode = housecodetoint(x10addr.slice(0, 1));
+    if (housecode >= 0) {
+        unitcode = x10addr.slice(1);
+        console.log(housecode.toString() + unitcode);
+        if (X10addrselected[housecode] === undefined) {
+            X10addrselected[housecode] = [];
+            X10addrselected[housecode][0] = 0;
+        }
+        else if (X10addrselected[housecode][0] === 1) {
+            delete X10addrselected[housecode];
+            X10addrselected[housecode] = [];
+            X10addrselected[housecode][0] = 0;
+        }
+        X10addrselected[housecode][unitcode] = 1;
+    }
+};
+
+function funcselected(housecode, func)
+{
+    if (X10addrselected[housecode] === undefined) {
+        X10addrselected[housecode] = [];
+    }
+    X10addrselected[housecode][0] = func;
+}
+
+function checkselected(housecode, unitcode)
+{
+    if (X10addrselected[housecode] === undefined) {
+        X10addrselected[housecode] = [];
+    }
+    return X10addrselected[housecode][unitcode];
+}
+
+exports.doselected = function(x10house, x10function)
+{
+    var housecode, unitcode, buttonupdate, i, x10addr;
+    housecode = housecodetoint(x10house.slice(0, 1));
+    buttonupdate = [];
+    if (housecode >= 0) {
+        switch (x10function) {
+            case 'On':
+                for (i = 1; i <= 16; i++) {
+                    if (checkselected(housecode, i) === 1) {
+                        x10addr = x10house + i.toString();
+                        this.on(x10addr);
+                        buttonupdate.push({buttonid: x10addr, state: 'on'});
+                    }
+                }
+                funcselected(housecode, 1);
+                break;
+            case 'Off':
+                for (i = 1; i <= 16; i++) {
+                    if (checkselected(housecode, i) === 1) {
+                        x10addr = x10house + i.toString();
+                        this.off(x10addr);
+                        buttonupdate.push({buttonid: x10addr, state: 'off'});
+                    }
+                }
+                funcselected(housecode, 1);
+                break;
+            case 'All_lights_off':
+            case 'All_units_off':
+                for (i = 1; i <= 16; i++) {
+                    x10addr = x10house + i.toString();
+                    buttonupdate.push({buttonid: x10addr, state: 'off'});
+                    this.off(x10addr);
+                }
+                funcselected(housecode, 1);
+                break;
+            case 'All_lights_on':
+                for (i = 1; i <= 16; i++) {
+                    x10addr = x10house + i.toString();
+                    buttonupdate.push({buttonid: x10addr, state: 'on'});
+                    this.on(x10addr);
+                }
+                funcselected(housecode, 1);
+                break;
+            default:
+                break;
+        }
+    }
+    return buttonupdate;
+};
+
+exports.deselect = function(x10addr)
+{
+    var housecode, unitcode;
+    housecode = housecodetoint(x10addr.slice(0, 1));
+    if (housecode >= 0) {
+        unitcode = x10addr.slice(1);
+        X10addrselected[housecode][unitcode]=0;
+    }
+};
 
 // Return an array with the on/off/unknown status of all X10 addresses.
 exports.now = function() 
@@ -29,21 +132,21 @@ exports.now = function()
     var h, i, x10addr, buttonupdate=[];
     // There are 16 house code A..P but do just A for now. Change "h < 1" to
     // "h < 16" for all 16 house codes.
-    for (h = 0; h < 1; h++) {
+    for (h = 0; h < 16; h++) {
         for (i = 1; i <= 16; i++) {
             x10addr = "ABCDEFGHIJKLMNOP".charAt(h) + i.toString();
             switch (x10state[x10addr]) {
                 case 'on': 
                     buttonupdate.push({buttonid: x10addr, state: 'on' });
-                    break;
+                break;
                 case 'off':
                     buttonupdate.push({buttonid: x10addr, state: 'off' });
-                    break;
+                break;
                 default:
                     buttonupdate.push({buttonid: x10addr, state: 'unknown' });
-                    break;
+                break;
             }
         }
     }
     return buttonupdate;
-}
+};
